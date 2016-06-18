920465a940037d50471d20663473d0cfd665b642
yambatrova.irina@gmail.com <yambatrova.irina@gmail.com>
2016-06-18 11:02:21 +0300

    no message

--- 11_1/11_1/11_1.cpp
+++ 11_1/11_1/11_1.cpp
@@ -1,39 +1,27 @@
-﻿/*Треугольник и точка (5)
-В декартовой системе координат на плоскости заданы координаты вершин треугольника и еще
-одной точки. Определить, принадлежит ли эта точка треугольнику.
-Ввод из файла INPUT.TXT. В четырех строках находятся пары чисел - координаты точек.
-Числа в первых трех строках - это координаты вершин треугольника, в четвертой строке -
-координаты тестируемой точки.
-Вывод в файл OUTPUT.TXT. Вывести In, если точка находится внутри треугольника, или Out -
-если снаружи.
-Ограничения: координаты вершин - целые числа, для любой точки выполняются следующие
-условия: -1000000000 ≤ x, y ≤ 100000000, время 1 с.
-*/
-
-#include "stdafx.h"
+﻿
 #include "stdafx.h"
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fstream>
 #include "11_1.h"
+#include <SFML/Graphics.hpp>
 
 using namespace std;
-
-int FindArea(int ax, int ay, int bx, int by, int cx, int cy)
+using namespace sf;
+int ModulVector(int ax, int ay, int bx, int by, int cx, int cy)
 {
-	return (abs((by - ay)*(cx - ax) - (bx - ax)*(cy - ay)))/2;
-	
+	return abs((by - ay)*(cx - ax) - (bx - ax)*(cy - ay));
 }
 
 bool CheckPoint(int ax, int ay, int bx, int by, int cx, int cy, int px, int py)
 {
-	int areaOfTriandle = FindArea(ax, ay, bx, by, cx, cy),
-		areaWithPoint1 = FindArea(ax, ay, bx, by, px, py),
-		areaWithPoint2 = FindArea(bx, by, cx, cy, px, py),
-		areaWithPoint3 = FindArea(cx, cy, ax, ay, px, py);
+	int modulVectorOfTriandle = ModulVector(ax, ay, bx, by, cx, cy),
+		modulVectorOfFirstTriangleWithPoint = ModulVector(ax, ay, bx, by, px, py),
+		modulVectorOfSecondTriangleWithPoint = ModulVector(bx, by, cx, cy, px, py),
+		modulVectorOfThirdTriangleWithPoint = ModulVector(ax, ay, cx, cy,  px, py);
 
-	if (areaWithPoint1 + areaWithPoint2 + areaWithPoint3 - areaOfTriandle > 0)
+	if (modulVectorOfFirstTriangleWithPoint + modulVectorOfSecondTriangleWithPoint + modulVectorOfThirdTriangleWithPoint - modulVectorOfTriandle > 0)
 	{
 		return false;
 	}
@@ -43,21 +31,67 @@ bool CheckPoint(int ax, int ay, int bx, int by, int cx, int cy, int px, int py)
 	}
 }
 
-void ReadInputAndOutputData(ifstream &inputf, ofstream &outputf)
+void Triangle(float ax, float ay, float bx, float by, float cx, float cy, float px, float py)
 {
-	
-	if (!inputf.is_open() || !outputf.is_open())
+	sf::RenderWindow window(sf::VideoMode(600, 600), "Triangle and Point");
+	sf::ConvexShape convex;
+
+	sf::CircleShape circle(2);
+
+	circle.setFillColor(sf::Color::Black);
+	circle.setPosition(px + 300, py + 300);
+
+	convex.setFillColor(sf::Color::Cyan);
+	convex.setPointCount(3);
+
+	convex.setPoint(0, sf::Vector2f(ax + 300, ay + 300));
+	convex.setPoint(1, sf::Vector2f(bx + 300, by + 300));
+	convex.setPoint(2, sf::Vector2f(cx + 300, cy + 300));
+
+	Font font;
+	font.loadFromFile("CyrilicOld.ttf");
+	Text text("", font, 35);
+	text.setColor(Color::Magenta);
+	text.setPosition(10, 400);
+
+	if (CheckPoint(ax, ay, bx, by, cx, cy, px, py))
 	{
-		cout << "Error: input.txt or output.txt is not open" << endl;
+		text.setString("Point in triangle ");
 	}
+	else
+	{
+		text.setString("Point not in triangle");
+	}
+	sf::Vertex line1[] =
+	{
+		sf::Vertex(sf::Vector2f(300, 0),sf::Color::Black),
+		sf::Vertex(sf::Vector2f(300, 600),sf::Color::Black)
+	};
 
-	int ax, ay, bx, by, cx, cy, px, py;
-	inputf >> ax >> ay  >> bx >> by >> cx >> cy >> px >> py;
-	outputf << (CheckPoint(ax, ay, bx, by, cx, cy, px, py) ? "In" : "Out");
+	sf::Vertex line2[] =
+	{
+		sf::Vertex(sf::Vector2f(0, 300),sf::Color::Black),
+		sf::Vertex(sf::Vector2f(600, 300),sf::Color::Black)
+	};
 
-	inputf.close();
-	outputf.close();
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear(sf::Color::White);
+		window.draw(line1, 2, sf::Lines);
+		window.draw(line2, 2, sf::Lines);
+
+		window.draw(convex);
+		window.draw(circle);
+		window.draw(text);
+		window.display();
+	}
 }
-
-
 
--- 11_1/11_1/11_1.h
+++ 11_1/11_1/11_1.h
@@ -1,12 +1,11 @@
 #pragma once
 #include "stdafx.h"
-#include "stdafx.h"
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fstream>
 #include <chrono>
 
-int FindArea(int ax, int ay, int bx, int by, int cx, int cy);
+int ModulVector(int ax, int ay, int bx, int by, int cx, int cy);
 bool CheckPoint(int ax, int ay, int bx, int by, int cx, int cy, int px, int py);
-void ReadInputAndOutputData(std::ifstream &inputf, std::ofstream &outputf);
+void Triangle(float ax, float ay, float bx, float by, float cx, float cy, float px, float py);
